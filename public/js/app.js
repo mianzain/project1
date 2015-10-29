16faf81f504889592a37d1c51ffa1e05cc469288
mianzain <zainahmed@Zains-MacBook-Air.local>
2015-10-28 17:03:55 -0700

    after linksn

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			.success(function (data) {
 				var name = data.comments[data.comments.length - 1].name;
 				var comment = data.comments[data.comments.length - 1].comments;
-				var html = "<li><span class='foodName'>" + name + "</span><span class='foodComment'>" + comment + "</span><li>"
+				var html = "<li><span class='foodName'>" + name + "</span><span class='foodComment'>" + comment + "</span><li>";
 				$("#"+data._id).append(html);
 			})
 			.error(function (data) {
@@ -28,21 +28,29 @@ $(document).ready(function(){
 $('#buttonSearch').click(function(){
 	userInput = $('#storage').val();
 	localStorage.setItem('userInput', userInput);
-	 });
+});
 
 
 	// var foodResults = $('div#searchResults');
 
 
-	$("#foodPost").validate({
-		name: {
-			required  : true,
-			minlength : 2 
-		},
-		message : {
-			name:"its to small"
-		}
-	});
+// 	$("#foodPost").validate({
+// 		name: {
+// 			required  : true,
+// 			minlength : 22
+// 		},
+	
+// 		comments: {
+
+// 			//required: true,
+// 			minlength : 10
+// 		}	
+	
+// // 			message : {
+// // 			name:"its to small"
+// // },
+
+// 	});
 
 });
 
